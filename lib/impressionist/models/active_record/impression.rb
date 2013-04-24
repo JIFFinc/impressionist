5f57b3a5aa108827f213fc6534f5f12b7fc575dd
Jon Fredeen <jon.fredeen@gmail.com>
2013-04-24 16:33:27 -0500

    Update impression.rb
    add user_agent to Impression attr_accessible

--- lib/impressionist/models/active_record/impression.rb
+++ lib/impressionist/models/active_record/impression.rb
@@ -1,7 +1,7 @@
 class Impression < ActiveRecord::Base
   attr_accessible :impressionable_type, :impressionable_id, :user_id,
   :controller_name, :action_name, :view_name, :request_hash, :ip_address,
-  :session_hash, :message, :referrer
+  :session_hash, :message, :referrer, :user_agent
 
   belongs_to :impressionable, :polymorphic=>true
 
